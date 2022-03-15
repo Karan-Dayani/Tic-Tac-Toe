f274d696421677e2cab3a40b00fbb0f11fc41d1d
Karan-Dayani <88270628+Karan-Dayani@users.noreply.github.com>
2022-03-15 13:24:53 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var playBtn = document.getElementById("play");
+var restartBtn = document.getElementById("restart-btn");
+var game = document.getElementById("game");
+var intro = document.getElementById("intro");
+
+playBtn.onclick = () => {
+    intro.classList.add("fadeOut");
+    game.classList.add("fadeIn");
+}
